3c5a8ffcd34cefddbc08aa3b4244bce277ab4697
D-xuanmo <me@xuanmo.xin>
2023-09-02 22:54:59 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  globals: {
+    MyGlobal: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    semi: 0,
+    quotes: ['error', 'single'],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true
+      }
+    ],
+
+    // ts
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'vue/valid-attribute-name': 0,
+
+    // vue
+    'vue/multi-word-component-names': 0,
+    'vue/no-v-html': 0,
+    'vue/max-attributes-per-line': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/html-self-closing': 0
+  }
+}
